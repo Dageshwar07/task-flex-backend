408a2c367c5e5f9e6e3228590cf73f5dbe32a7c3
Dageshwar07 <dageshwardasmanikpuri996@gmail.com>
2025-05-17 18:41:37 +0530

    upload image worked

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const uploadRoutes = require("./routes/uploadRoutes")
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // Fixed duplicate entry
+    origin: ["https://task-manager-blond-eight.vercel.app", "http://localhost:5174"], // Fixed duplicate entry
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
